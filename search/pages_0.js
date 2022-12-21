ee79514caf39cac2c2dc9ce448f9e13b7ebd7c22
Vivek-NatWest <Vivek-NatWest@users.noreply.github.com>
2022-12-21 10:34:46 +0000

    deploy: 2da19ce009a859aa1990f0f5dbc731f613ec517c

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['backplane_0',['Backplane',['../index.html',1,'(Global Namespace)'],['../md_docs_backplane.html',1,'(Global Namespace)'],['../md_src__finos__fdc3__backplane__readme.html',1,'(Global Namespace)']]],
-  ['backplane_20client_20_2enet_1',['Backplane Client .NET',['../md_docs_backplane_client_net.html',1,'(Global Namespace)'],['../md_src__finos__fdc3__backplane__client__readme.html',1,'(Global Namespace)']]],
-  ['backplane_20client_20js_2',['Backplane Client JS',['../md_docs_backplane_client_js.html',1,'']]],
-  ['backplane_20contribution_20and_20governance_20policies_3',['Backplane Contribution and Governance Policies',['../md__c_o_n_t_r_i_b_u_t_i_n_g.html',1,'']]]
+  ['backplane_0',['Backplane',['../index.html',1,'']]],
+  ['backplane_20contribution_20and_20governance_20policies_1',['Backplane Contribution and Governance Policies',['../md__c_o_n_t_r_i_b_u_t_i_n_g.html',1,'']]]
 ];
