0c392aff24ed652aa68fd69f2624b377e1e74a72
Vivek-NatWest <Vivek-NatWest@users.noreply.github.com>
2023-01-07 08:07:46 +0000

    deploy: 3a3e3a67282d612ff5ae014cce3376f542946b54

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['backplane_20contribution_20and_20governance_20policies_0',['Backplane Contribution and Governance Policies',['../md__c_o_n_t_r_i_b_u_t_i_n_g.html',1,'']]]
+  ['backplane_20contribution_20and_20governance_20policies_0',['Backplane Contribution and Governance Policies',['../md__c_o_n_t_r_i_b_u_t_i_n_g.html',1,'']]],
+  ['backplane_2dclient_2dnet_2dv1_2e0_2e0_2dalpha_2e1_20_282023_2d01_2d07_29_1',['backplane-client-net-v1.0.0-alpha.1 (2023-01-07)',['../md_src__finos__fdc3__backplane__client__c_h_a_n_g_e_l_o_g.html',1,'']]]
 ];
