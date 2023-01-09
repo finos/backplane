303c318f5c18f39b47ed2c59a9264934f588ff73
Vivek-NatWest <Vivek-NatWest@users.noreply.github.com>
2023-01-09 11:42:38 +0000

    deploy: d2ea4f2d3187b4c25dbca1cedb2a17c8e5b10af1

--- search/pages_0.js
+++ search/pages_0.js
@@ -3,5 +3,5 @@ var searchData=
   ['backplane_0',['Backplane',['../md_src__finos__fdc3__backplane__readme.html',1,'']]],
   ['backplane_20client_20_2enet_1',['Backplane Client .NET',['../md_src__finos__fdc3__backplane__client__readme.html',1,'']]],
   ['backplane_20contribution_20and_20governance_20policies_2',['Backplane Contribution and Governance Policies',['../md__c_o_n_t_r_i_b_u_t_i_n_g.html',1,'']]],
-  ['backplane_2dclient_2dnet_2dv1_2e0_2e0_2dalpha_2e1_20_282023_2d01_2d07_29_3',['backplane-client-net-v1.0.0-alpha.1 (2023-01-07)',['../md_src__finos__fdc3__backplane__client__c_h_a_n_g_e_l_o_g.html',1,'']]]
+  ['backplane_2dclient_2dnet_2dv1_2e0_2e0_2dbeta_2e1_20_282023_2d01_2d09_29_3',['backplane-client-net-v1.0.0-beta.1 (2023-01-09)',['../md_src__finos__fdc3__backplane__client__c_h_a_n_g_e_l_o_g.html',1,'']]]
 ];
